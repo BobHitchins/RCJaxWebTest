d4e4b77d37628c1e17ec9748ae829a364e80af00
bobh <robert.hitchins@gmail.com>
2019-11-05 23:07:34 -0500

    Added banner and some graphics

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LeftNavComponent } from './left-nav/left-nav.component';
+import { BannerComponent } from './banner/banner.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LeftNavComponent
+    LeftNavComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
